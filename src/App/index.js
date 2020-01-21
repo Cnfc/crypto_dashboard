5027fd66bc82370a79b9ddf126f87a70467e8c04
Cnfc <stdashkov@mail.ru>
2020-01-21 14:53:18 +0300

    Add AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Dashboard
-    </div>
-  );
+import AppBar from './AppBar';
+
+import AppLayout from './AppLayout';
+import styled, { css } from 'styled-components';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <AppLayout>
+        <AppBar />
+        <WelcomeMessage />
+      </AppLayout>
+    );
+  }
 }
 
 export default App;
