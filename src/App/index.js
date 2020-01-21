b7bc38cedc58020eb6cd4ca344afa721e2f12dac
Cnfc <stdashkov@mail.ru>
2020-01-21 16:18:03 +0300

    AppProvider

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import './App.css';
+import { AppProvider } from './AppProvider';
 
 import AppBar from './AppBar';
-
 import AppLayout from './AppLayout';
-import styled, { css } from 'styled-components';
+
 
 
 class App extends Component {
@@ -13,8 +13,10 @@ class App extends Component {
   render() {
     return (
       <AppLayout>
-        <AppBar />
-        <WelcomeMessage />
+        <AppProvider>
+          <AppBar />
+          <WelcomeMessage />
+        </AppProvider>
       </AppLayout>
     );
   }
