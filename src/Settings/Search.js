04eca288da735a375f8b232a77026ad4cc62f862
Cnfc <stdashkov@mail.ru>
2020-01-22 14:06:12 +0300

    add filtered coins

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,53 +1,60 @@
 import React from 'react';
-import { AppContext } from '../App/AppProvider';
-import styled, { css } from 'styled-components';
-import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+import { AppContext } from "../App/AppProvider";
+import styled from 'styled-components';
+import { backgroundColor2, fontSize2 } from "../Shared/Styles";
 import _ from 'lodash';
 import fuzzy from 'fuzzy';
 
-
 const SearchGrid = styled.div`
-  dispay: grid;
-  grid-template-columns: 200px, 1fr;
-`;
+  display: grid; 
+  grid-template-columns: 200px 1fr; 
+`
 
 const SearchInput = styled.input`
   ${backgroundColor2}
   ${fontSize2}
-  border: 1px solid;
-  heigth: 25px;
+  border: 1px solid; 
+  height: 25px; 
   color: #1163c9;
-  place-self: center left;
-`;
+  place-self: center left; 
+`
 
-
-const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
+const handleFilter = _.debounce((inputValue, coinList, setFilterCoins) => {
+  // Get all the coin symbols
   let coinSymbols = Object.keys(coinList);
-
+  // Get all the coin names, map symbol to name
   let coinNames = coinSymbols.map(sym => coinList[sym].CoinName)
-  let allStringToSearch = coinSymbols.concat(coinNames);
+  let allStringsToSearch = coinSymbols.concat(coinNames);
+  let fuzzyResults = fuzzy
+    .filter(inputValue, allStringsToSearch, {})
+    .map(result => result.string);
 
-  let fuzzyResult = fuzzy.filter(inputValue, allStringToSearch, {});
-  console.log(fuzzyResult)
+  let filteredCoins = _.pickBy(coinList, (result, symKey) => {
+    let coinName = result.CoinName;
+    return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName));
+  });
+  setFilterCoins(filteredCoins);
 
 }, 500);
 
 function filterCoins(e, setFilteredCoins, coinList) {
   let inputValue = e.target.value;
-  handleFilter(e, setFilteredCoins, coinList)
-
+  if (!inputValue) {
+    setFilteredCoins(null);
+    return;
+  }
+  handleFilter(inputValue, coinList, setFilteredCoins);
 }
 
 export default function () {
   return (
     <AppContext.Consumer>
       {({ setFilteredCoins, coinList }) =>
-
         <SearchGrid>
-          <h2>Search all Coins</h2>
+          <h2>Search all coins</h2>
           <SearchInput onKeyUp={(e) => filterCoins(e, setFilteredCoins, coinList)} />
         </SearchGrid>
       }
     </AppContext.Consumer>
-  )
+  );
 }
